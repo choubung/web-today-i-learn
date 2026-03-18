f076be4d6a89fdd5ddcece970e5cfe84fcd2772c
choubung <choubung@sookmyung.ac.kr>
2026-03-18 14:45:15 +0900

    Feat: 폼 저장 기능 구현과 페이지 내용 채우기

--- js/index.js
+++ js/index.js
@@ -1,12 +1,47 @@
-// TODO: TIL 폼 등록 기능을 구현하세요
-// 1. 폼 요소와 목록 요소를 querySelector로 선택합니다.
-// 2. 폼의 submit 이벤트를 감지하여 새 TIL 항목을 목록에 추가합니다.
-
 const tilForm = document.querySelector("#til-form");
 const tilList = document.querySelector("#til-list");
+
+function init() {
+  const savedTils = JSON.parse(localStorage.getItem("myTils")) || [];
+
+  savedTils.forEach((til) => {
+    renderTil(til);
+  });
+}
+
+function renderTil(tilData) {
+  const newArticle = document.createElement("article");
+  newArticle.classList.add("til-item");
+
+  newArticle.innerHTML = `
+    <time>${tilData.date}</time>
+    <h3>${tilData.title}</h3>
+    <p>${tilData.content.replace(/\n/g, "<br>")}</p>
+  `;
+
+  tilList.prepend(newArticle);
+}
 
 tilForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  // TODO: 입력값을 가져와서 새 TIL 항목을 만들어 목록에 추가하세요
+  const date = document.querySelector("#til-date").value;
+  const title = document.querySelector("#til-title").value;
+  const content = document.querySelector("#til-content").value;
+
+  const newTil = {
+    date: date,
+    title: title,
+    content: content,
+  };
+
+  const currentTils = JSON.parse(localStorage.getItem("myTils")) || [];
+  currentTils.push(newTil);
+  localStorage.setItem("myTils", JSON.stringify(currentTils));
+
+  renderTil(newTil);
+
+  tilForm.reset();
 });
+
+init();
